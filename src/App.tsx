4fecead1556073ded6d9543659ac7f1dd763c82c
John Chen <john.data.chen@gmail.com>
2024-10-30 19:52:26 +0800

    Add the reset in form

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import './App.css';
-import TaskList from './components/TaskList';
-import { useState } from 'react';
-import { Task } from './types';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import "./App.css";
+import TaskList from "./components/TaskList";
+import { useState } from "react";
+import { Task } from "./types";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const initialTasks: Task[] = [
-  { id: 1, title: 'example task', status: 'To Do', dueDate: new Date(), owner: 'me', description: 'delete me if you want' },
+  {
+    id: 1,
+    title: "example task",
+    status: "To Do",
+    dueDate: new Date(),
+    owner: "me",
+    description: "delete me if you want",
+  },
 ];
 
 interface FormInput {
-  assignee: string;
   title: string;
   owner: string;
   description: string;
@@ -21,29 +29,36 @@ interface FormInput {
 
 function App() {
   const [tasks, setTasks] = useState(initialTasks);
-  const { register, handleSubmit, formState: { errors } } = useForm<FormInput>();
+  const { handleSubmit, control, reset } = useForm<FormInput>({
+    defaultValues: {
+      title: "",
+      owner: "",
+      description: "",
+      status: "To Do",
+      dueDate: new Date(),
+    },
+  });
 
   const onSubmit: SubmitHandler<FormInput> = (data) => {
-    try {
     const newTask = {
       id: tasks.length + 1,
       title: data.title,
       owner: data.owner,
       description: data.description,
       status: data.status,
-      dueDate: data.dueDate ? new Date(data.dueDate) : new Date(),
+      dueDate: data.dueDate,
     };
     setTasks([...tasks, newTask]);
-  } catch (error) {
-    console.error(error);
-    toast.error('Error adding task');
-  }
+    toast.success("Task added successfully");
+    reset();
   };
 
   const handleDeleteTask = (taskId: number) => {
     toast.promise(
       new Promise((resolve) => {
-        const confirmDelete = window.confirm('Are you sure you want to delete this task?');
+        const confirmDelete = window.confirm(
+          "Are you sure you want to delete this task?"
+        );
         if (confirmDelete) {
           const newTasks = tasks.filter((task) => task.id !== taskId);
           setTasks(newTasks);
@@ -53,17 +68,17 @@ function App() {
         }
       }),
       {
-        pending: 'Deleting task...',
+        pending: "Deleting task...",
         success: {
           render({ data }) {
             if (data) {
-              return 'Task deleted successfully';
+              return "Task deleted successfully";
             } else {
-              return 'Cancel deleting task';
+              return "Cancel deleting task";
             }
           },
         },
-        error: 'Error deleting task',
+        error: "Error deleting task",
       }
     );
   };
@@ -73,43 +88,57 @@ function App() {
       <h1>Today main task</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="title">Title (required)</label>
-        <input
-          {...register('title', { required: true })}
-          placeholder="Enter task title"
-          required
-          id = "title"
+        <Controller
+          name="title"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => <input {...field} />}
         />
-        {errors.title?.message && <p>{errors.title.message}</p>}
         <label htmlFor="owner">Task Owner</label>
-        <input
-          {...register('owner')}
-          placeholder="Enter Task Owner"
+        <Controller
+          name="owner"
+          control={control}
+          render={({ field }) => <input {...field} />}
         />
         <label htmlFor="description">Description</label>
-        <textarea
-          {...register('description')}
-          placeholder="Enter description"
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => <input {...field} />}
         />
         <label htmlFor="status">Status</label>
-        <select {...register('status')}>
-          <option value="todo">To Do</option>
-          <option value="doing">Doing</option>
-          <option value="done">Done</option>
-        </select>
-        <label htmlFor="due-date">Due Date</label>
-        <input
-          {...register('dueDate')}
-          type="date"
-          placeholder="Enter due date"
-          min = {new Date().toISOString().split('T')[0]}
+        <Controller
+          name="status"
+          control={control}
+          render={({ field }) => (
+            <select {...field}>
+              <option value="To Do">To Do</option>
+              <option value="In Progress">In Progress</option>
+              <option value="Done">Done</option>
+            </select>
+          )}
         />
+        <label htmlFor="dueDate">Due Date</label>
+        <Controller
+          control={control}
+          name="dueDate"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <ReactDatePicker
+              selected={value}
+              onChange={onChange}
+              dateFormat="yyyy/MM/dd"
+              onBlur={onBlur}
+              minDate={new Date()}
+            />
+          )}
+        />
+
         <button type="submit">Add Task</button>
       </form>
-      <TaskList tasks={tasks} onDelete={handleDeleteTask}/>
+      <TaskList tasks={tasks} onDelete={handleDeleteTask} />
       <ToastContainer />
     </>
   );
 }
-
 
 export default App;
