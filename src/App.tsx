4f4d377101963f4c6c4d2daafd3c02a57369da76
John Chen <john.data.chen@gmail.com>
2024-10-30 14:44:32 +0800

    add a prototype of to-do list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import './App.css'
+import './App.css';
+import TaskList from './components/TaskList';
+import { useState } from 'react';
+
+interface Task {
+  id: number;
+  text: string;
+}
+
+const initialTasks: Task[] = [
+  { id: 1, text: 'example task, delete me if you want' },
+];
 
 function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+  const [newTaskTitle, setNewTaskTitle] = useState('');
+
+  const handleAddTask = () => {
+    const newTask : Task = {
+      id: tasks.length + 1,
+      text: newTaskTitle,
+    };
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
+  };
 
   return (
     <>
       <h1>Today main task</h1>
+      <input type="text" value={newTaskTitle} onChange={(e) => setNewTaskTitle(e.target.value)} />
+      <button onClick={handleAddTask}>Add Task</button>
+      <TaskList tasks={tasks} />
     </>
-  )
+  );
 }
 
-export default App
+
+export default App;
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface Task {
+  id: number;
+  text: string;
+}
+
+interface TaskListProps {
+  tasks: Task[];
+}
+
+const TaskList: React.FC<TaskListProps> = ({ tasks }) => {
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <li key={task.id}>{task.text}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default TaskList;
