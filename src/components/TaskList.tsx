899a92c12e95e45a990445fe8a469d32e1bca101
John Chen <john.data.chen@gmail.com>
2024-10-31 20:19:12 +0800

    Add a fake data

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -9,6 +9,18 @@ interface TaskListProps {
 }
 
 const TaskList: React.FC<TaskListProps> = ({ tasks, onDelete }) => {
+  const defaultTask: TaskFormType = {
+    id: 1,
+    title: "Default Task",
+    owner: "John Doe",
+    status: "To-Do",
+    dueDate: new Date(),
+    description: "This is a default task",
+  };
+
+  if (tasks.length === 0) {
+    tasks = [defaultTask];
+  }
   return (
     <Box>
       <Heading mb={4}>Task List</Heading>
